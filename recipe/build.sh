a3cb53587ebf9396a9208b0faafe430862bf0bdf
v-turin <vladimir.turin@opencascade.com>
2018-11-08 14:40:22 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 mkdir build && cd build
 
-export LDFLAGS="$LDFLAGS -lz -lexpat"
+export LDFLAGS="$LDFLAGS -fuse-ld=gold"
 
 cmake -G "Ninja"                        \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}    \
+    -DCMAKE_PREFIX_PATH=${PREFIX}       \
     -DCMAKE_BUILD_TYPE=Release          \
     ..
 
